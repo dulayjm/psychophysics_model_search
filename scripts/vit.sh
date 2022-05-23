eede4656f6640a303887c347b06f1fa249cb81ce
jdulay <jdulay@nd.edu>
2022-05-23 18:35:21 -0400

    fixed the dumbest typo ever

--- scripts/vit.sh
+++ scripts/vit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#$ -N vit_imagenet
+#$ -N vit_debug
 #$ -q gpu
 #S -m abe
-#$ -l gpu=8
+#$ -l gpu=1
 
 BASE_PATH="$HOME/research/psychophysics_model_search" 
 
 source nas_env/bin/activate
 # pip3 install -r requirements.txt
 
-python3 ViT.py --model_name="ViT" --dataset_name="imagenet" --loss_fn="else" --log=True --batch_size=16
+python3 vit.py --model_name="ViT" --dataset_name="imagenet" --loss_fn="else" --log=True --batch_size=16
